e99a36417556c4a75b49ce4dc5265f96f91c2a3c
frontendowl <frontendowl@gmail.com>
2021-09-27 14:51:43 +0100

    Add top up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,4 +3,9 @@ class Oystercard
   def initialize
     @balance = 0
   end
+
+  def top_up(n)
+    @balance += n 
+    return "New balance: #{@balance}"
+  end  
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Oystercard do
   it "initialized with balance 0" do
     expect(subject.balance).to eq 0 
   end
+
+  it "add money to the card" do
+    expect(subject.top_up(100)).to eq "New balance: 100"
+  end
 end
